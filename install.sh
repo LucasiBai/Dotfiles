6ec4ea1cd7ea8ddb3904199317e144004ca7e191
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2023-08-27 19:05:37 -0300

    actualizacion dls

--- install.sh
+++ install.sh
@@ -1,23 +1,21 @@
-#!/bin/dash
+#!/bin/sh
 
 RUTA=$(pwd)
 
-sudo apt update
+sudo pacman -Syu
 
-sudo apt upgrade
-
-sudo apt install feh plymouth lightdm rofi dunst
-sudo apt install alacritty picom
+sudo pacman -S feh plymouth lightdm rofi dunst
+sudo pacman -S alacritty picom
 
 yay -S betterlockscreen
 
-sudo pip install qtile
+sudo pacman -S qtile
 
 sudo bash -c 'cat > /usr/share/xsessions/qtile.desktop <<EOF
 [Desktop Entry]
 Name=Qtile
 Comment=Qtile Session
-Exec=/usr/local/bin/qtile start
+Exec=/usr/bin/qtile start
 Type=Application
 Keywords=wm;tiling
 EOF'
